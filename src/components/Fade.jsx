91f8f15240478dc13e084baaac4bed6248d06572
GAGGAH1 <titusdcstd2022@gmail.com>
2025-05-26 15:12:27 +0100

    Removed the Overlay colour of the images

--- src/components/Fade.jsx
+++ src/components/Fade.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import pic1 from '../assets/images/NigeriaSat-1-with-KHTT-team.png';
-import pic2 from '../assets/images/astronaaut-7550543_1280.png'; // ✅ Replace with your actual path
-import pic3 from '../assets/images/NigeriaSat2_with_engineers.png'; // ✅ Replace with your actual path
+import pic2 from '../assets/images/astronaaut-7550543_1280.png';
+import pic3 from '../assets/images/NigeriaSat2_with_engineers.png';
 import pic4 from '../assets/images/Eng-Sadq-Umar.jpg';
+
 const Fade = () => {
   const images = [pic1, pic2, pic3, pic4];
   const [activeImage, setActiveImage] = useState(0);
@@ -17,7 +18,6 @@ const Fade = () => {
 
   return (
     <div className="w-full h-screen relative overflow-hidden">
-
       {images.map((image, index) => (
         <img
           key={index}
@@ -27,11 +27,10 @@ const Fade = () => {
             activeImage === index ? 'opacity-100' : 'opacity-0'
           }`}
         />
-      ))};
-      
+      ))}
 
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-black bg-opacity-70 px-4">
-        <h1 className="text-3xl md:text-5xl font-bold mb-8  rounded-lg p-2 bg-opacity-90">
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4">
+        <h1 className="text-3xl md:text-5xl font-bold mb-8 rounded-lg p-2">
           ABOUT US
         </h1>
       </div>
