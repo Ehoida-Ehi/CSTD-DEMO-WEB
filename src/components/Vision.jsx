aaa2df32cb8248a4da4ab8e529652550d4edbe86
GAGGAH1 <titusdcstd2022@gmail.com>
2025-05-26 10:53:08 +0100

    We changed font,colour and background colour

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Vision = () => {
   return (
-   <div className="p-6 bg-gray-100 rounded-lg shadow-md mt-4">
-      <h2 className="text-2xl font-bold mb-4 text-green-600">VISION</h2>
+   <div className="p-6 bg-white text-center rounded-lg shadow-md mt-4">
+      <h2 className="text-4xl font-bold mb-4 text-green-700">VISION</h2>
       
       <p className="text-gray-700 leading-relaxed">
         To attain continental leadership in satellite technology through the development of competence and capabilities in satellite subsystems of various missions as a tool for national growth, innovation and contribution to the socio-economic benefits of Nigerians and humanity.
